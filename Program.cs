166d187bc437f15e253eed3dbc60300db33c113d
JoaoAlexandre <joaoalexandre1807@gmail.com>
2026-04-30 14:24:47 -0300

    Todo's realizados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("912345678", "Iphone12", "123456789101112", 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Angry Birds");
+
+
+Nokia nokia = new Nokia("987654321", "Tijolão", "121110987654321", 1);
+nokia.Ligar();
+nokia.InstalarAplicativo("SMS");
